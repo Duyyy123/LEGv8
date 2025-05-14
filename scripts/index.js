d0041511acd3dc2721c00b04dd124eaf7420eb0c
Duyy123 <bojack@Bojack.>
2025-05-14 23:44:33 +0700

    try canvas tag

--- scripts/index.js
+++ scripts/index.js
@@ -188,3 +188,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 });
+
+const c = document.getElementById("mainCanvas");
+const ctx = c.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 75, 50);
+
+//Turn transparency on
+ctx.globalAlpha = 0.2;
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 75, 50);
+ctx.fillStyle = "green";
+ctx.fillRect(80, 80, 75, 50);
